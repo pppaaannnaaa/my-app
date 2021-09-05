ca1c339a958abe7b9f57931ad65cf09d27d38f27
pppaaannnaaa <pppaaannnaaa@gmail.com>
2021-09-05 12:40:19 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            </ul>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
